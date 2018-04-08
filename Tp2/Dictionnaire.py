e708970c90de1cd558d3879249414b7aa8cc5ae2
len3158 <32147167+len3158@users.noreply.github.com>
2018-04-08 14:29:16 -0400

    Cleaning up
    -Reste a revoir les commentaires, mais tu peux run cette version (c'est long!)

---
+++ Tp2/Dictionnaire.py
@@ -0,0 +1,141 @@
+"""
+Notre classe dictionnaire
+
+En utilisant la méthode de Horner pour la fonction de hashage:
+https://en.wikipedia.org/wiki/Horner%27s_method
+
+"""
+from random import randrange
+from Bucket import Bucket
+class Dictionnaire:
+	def __init__(self,cap = 786433, p = 109345121):
+		self._taille = cap
+		self._Tableau = self._taille * [None] #taille par defaut lors de l'appel
+		self._premier = p				#nombre premier pour compression MAD
+		self._coefficient = 33		#33 peut-être évaluer autre valeur? à voir
+		self._echelle = 1 + randrange(self._premier-1)	#echelle MAD
+		self._shift = randrange(self._premier)	#décalage pour MAD
+		self._nbElem = 0
+		self._keys = Bucket()	 #keep track of all inserted keys pour la distance
+		self._index = Bucket()		#List des index à utiliser pour la methode items
+		self._indice_premier = 0 #keep track of prime number in prime list
+		
+	def __len__(self):
+		return len(self._Tableau)
+		
+	"""Inserer valeur associee a la cle k dans la hashTable"""
+	def __setitem__(self, k, v):
+		iterateur = self._hash_(k)
+		i = self._compress(iterateur)
+		existe = self._bucket_setitem(i, k, v)
+		if self._nbElem > len(self._Tableau)//2:
+			self._resize(2*len(self._Tableau)-1)
+		if not existe:
+			self._keys.append(k)
+			self._nbElem += 1
+	
+	"""Inserer une clée sans valeur associee a la cle k dans la hashTable"""
+	def insert(self, k):
+		iterateur = self._hash_(k)
+		i = self._compress(iterateur)
+		existe = self._bucket_insert(i, k)
+		if self._nbElem > len(self._Tableau)//2:
+			self._resize(2*len(self._Tableau)-1)
+		if not existe:
+			self._keys.append(k)
+			self._nbElem += 1
+			
+	"""Generate a sequence of key in the map"""
+	def __iter__(self):
+		for key in self._keys:
+			yield key
+
+	"""Generate a sequence of (k,v) tuples for all entries of M"""
+	def __items__(self):
+		for i in self._index:
+			yield list(self._Tableau[i].__items__())
+			
+	def keys(self):
+		return self._keys
+	
+	
+	def __contains__(self,k):
+		value = self.__getitem__(k)
+		if value is None:
+			return False
+		else:
+			return True
+	
+	def __str__(self):	
+		pp = "Taille de table: "+ str(self._taille) + "\n"
+		for i in self._index:
+			pp += "Index: " + str(i)
+			pp += str(self._Tableau[i])
+			pp += "\n"
+		return pp
+	
+	
+	"""Retourner la valeur associee a la cle k"""
+	def __getitem__(self, k):
+		iterateur = self._hash_(k)
+		i = self._compress(iterateur)
+		return self._bucket_getitems(i, k)
+		
+	def _bucket_getitems(self,i,k):
+		bucket = self._Tableau[i]
+		if bucket is not None:
+			return bucket[k]
+		return None		#If bucket is None
+			
+	def _bucket_setitem(self,i,k,v):
+		if self._Tableau[i] is None:
+			self._Tableau[i] = Bucket()
+			self._index.append(i)
+		return self._Tableau[i]._setitem(k,v)	
+	
+
+	def _bucket_insert(self, i,k):
+		if self._Tableau[i] is None:
+			self._Tableau[i] = Bucket()
+			self._index.append(i)
+		return self._Tableau[i].insert(k)
+
+			
+	"""Grows the table as more keys are added
+		http://www.orcca.on.ca/~yxie/courses/cs2210b-2011/htmls/extra/PlanetMath_%20goodhashtable.pdf"""
+	def _resize(self, newSize):
+		listePremiers = [7, 53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469, 12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610612741]
+		i = listePremiers.index(self._taille)
+		while i < len(listePremiers):
+			if self._nbElem > (len(self._Tableau)//2):
+				old = list(self.__items__())
+				self._index = Bucket()
+				self._keys = Bucket()
+				self._Tableau = listePremiers[i+1] * [None]
+				self._taille = listePremiers[i+1]
+				self._premier = listePremiers[i+1]
+				self._nbElem = 0
+				if old is not None:
+					for ss in old:
+						for tt in ss:
+							self.__setitem__(tt[0],tt[1])
+			
+			return
+			#break
+		
+		
+	def _hash_(self,k):
+			return self._horner_method(k)
+	
+	"""Fonction de hashage"""
+	def _horner_method(self, k):
+		if len(k) <= 1:
+			return ord(k[0])
+		else:
+			return  ord(k[0]) + self._coefficient*self._horner_method(k[1:])	#ord retourne le char en entier (coefficient)
+		
+	"""En utilisant la methode MAD"""
+	def _compress(self, hash_value):
+		return (hash_value*self._echelle+self._shift)%self._premier%len(self._Tableau)
+	
+	
---
+++ Tp2/Tp2_1055234_1047837.py
@@ -0,0 +1,151 @@
+from Dictionnaire import Dictionnaire
+import time
+import math
+
+PONC = ["!",'"',"'",")","(",",",".",";",":","?", "-", "_"]
+"""Represents text with doublet frequencies."""
+def treatText(table, filename):
+	prevword = None
+	with open(filename,encoding="utf8") as f:
+		lines = f.readlines()
+		prevLine = None
+		for line in lines:
+			line = line.rstrip()
+			if line == '': continue
+			words = treatLine(line)
+			for item in range(len(words)):
+				if prevLine is not None:
+					table.insert(prevLine+" "+words[item])
+					prevLine = None
+					pass
+				try:
+					if words[item+1] is not None:
+						#print(words[item]+" "+words[item+1])
+						table.insert(words[item]+" "+words[item+1])
+				except IndexError:
+					prevLine = words[item]
+					pass
+
+def treatLine(line):
+	"""
+	Separates words and removes punctuation.
+	"""
+	noponc = ""
+	for c in line:
+		if c in PONC:
+			noponc = noponc + " "
+		else:
+			noponc = noponc + c
+	words = noponc.split()
+	wlower = []
+	for w in words:
+		if len(w) > 2:
+			wlower.append(w.lower())
+	return wlower
+
+def dist_between_ds(d1, d2):
+	commond = dict()
+	thisn = 0
+	othern = 0
+	dist = 0
+	for k in d1._keys:
+		x = d1[k]
+		if k in d2:
+			y = d2[k]
+			commond[k] = (x, y)
+			thisn += x
+			othern += y
+	for k in d2._keys:
+		if k in commond:
+			continue
+		y = d2[k]
+		if k in d1:
+			x = d1[k]
+			commond[k] = (x, y)
+			thisn += x
+			othern += y
+	for k in commond.keys():
+		x = commond[k]
+		dist += (x[0]/thisn - x[1]/othern)**2
+	if not len(commond.keys()) == 0:
+		dist = dist / len(commond.keys())
+		dist = math.sqrt(dist)
+		return dist
+	else:
+		return dist
+
+def deviner_Texte():
+	resultats = []
+	tableTest = Dictionnaire()
+	treatText(tableTest,"test.txt")
+	tableVerne = Dictionnaire()
+	treatText(tableVerne, "verne.txt")
+	tableZola = Dictionnaire()	
+	treatText(tableZola, "zola.txt")
+	tableBalzac = Dictionnaire()
+	treatText(tableBalzac, "balzac.txt")
+	tableHugo = Dictionnaire()
+	treatText(tableHugo,"hugo.txt")
+	tableSegur = Dictionnaire()
+	treatText(tableSegur, "segur.txt")
+	tableVoltaire = Dictionnaire()
+	treatText(tableVoltaire, "voltaire.txt")
+	tableMystere = Dictionnaire()
+	treatText(tableMystere, "mystere.txt")
+
+	resultats.append([dist_between_ds(tableVerne, tableMystere), "Verne: "])
+	resultats.append([dist_between_ds(tableZola, tableMystere), "Zola: "])
+	resultats.append([dist_between_ds(tableHugo, tableMystere), "Hugo: "])
+	resultats.append([dist_between_ds(tableSegur, tableMystere), "Segur: "])
+	resultats.append([dist_between_ds(tableVoltaire, tableMystere), "Voltaire: "])
+	resultats.append([dist_between_ds(tableBalzac, tableMystere), "Balzac: "])
+	for i in resultats:
+		print(i[1]+" "+str(round(i[0], 5)))
+	value = min(resultats)
+	print("Auteur du texte mystère: "+value[1])
+	
+def test_performance():		
+	start_time = time.time()
+	tableTest = Dictionnaire()
+	treatText(tableTest,"test.txt")
+	print("Temps importation du texte de Test: " + str(round(time.time() - start_time,3))+" secondes")
+	
+	start_time = time.time()
+	tableVerne = Dictionnaire()
+	treatText(tableVerne, "verne.txt")
+	print("Temps importation du texte de Verne: " + str(round(time.time() - start_time,3))+" secondes")
+
+	start_time = time.time()
+	tableZola = Dictionnaire()	
+	treatText(tableZola, "zola.txt")
+	print("Temps importation du texte de Zola: " + str(round(time.time() - start_time,3))+" secondes")
+
+	start_time = time.time()
+	tableBalzac = Dictionnaire()
+	treatText(tableBalzac, "balzac.txt")
+	print("Temps importation du texte de Balzac: " + str(round(time.time() - start_time,3))+" secondes")
+
+	start_time = time.time()
+	tableHugo = Dictionnaire()
+	treatText(tableHugo,"hugo.txt")
+	print("Temps importation du texte de Hugo: " + str(round(time.time() - start_time,3))+" secondes")
+
+	start_time = time.time()
+	tableSegur = Dictionnaire()
+	treatText(tableSegur, "segur.txt")
+	print("Temps importation du texte de Segur: " + str(round(time.time() - start_time,3))+" secondes")
+
+	start_time = time.time()
+	tableVoltaire = Dictionnaire()
+	treatText(tableVoltaire, "voltaire.txt")
+	print("Temps importation du texte de Voltaire: " + str(round(time.time() - start_time,3))+" secondes")
+
+	start_time = time.time()
+	tableMystere = Dictionnaire()
+	treatText(tableMystere, "mystere.txt")
+	print("Temps importation du texte mystere: " + str(round(time.time() - start_time,3))+" secondes")
+
+
+if __name__=="__main__":
+	deviner_Texte()
+	#test_performance()
